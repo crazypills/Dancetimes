91adadfb6b75618aa96faac81303c80e6387cd0b
John Carrino <carrino@palantir.com>
2016-04-05 23:45:22 -0700

    test out dancing

--- Fast_LEDs_combo_w__Accel_code/Fast_LEDs_combo_w__Accel_code.ino
+++ Fast_LEDs_combo_w__Accel_code/Fast_LEDs_combo_w__Accel_code.ino
@@ -66,7 +66,7 @@ void loop() {
 //      //Stick.ColorSet(Stick.Color(255, 0, 0));
 //      if (Stick.ActivePattern != FOLLOWER) {
 //        Stick.Follower(Stick.Color(0,255,0),40,2);
-        rainbowWithGlitter();
+        computed_bpm(phase.getPhasePercentage() * 256);
      }
     
 //      Stick.Update();
@@ -161,6 +161,16 @@ void bpm()
   uint8_t BeatsPerMinute = 62;
   CRGBPalette16 palette = PartyColors_p;
   uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < NUM_LEDS; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
+
+void computed_bpm(uint8_t beat)
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  CRGBPalette16 palette = PartyColors_p;
+  //uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
   for( int i = 0; i < NUM_LEDS; i++) { //9948
     leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
   }
