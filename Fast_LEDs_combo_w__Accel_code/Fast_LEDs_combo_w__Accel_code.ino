0c3da7f5af48a4d39f5ac14952b247a1f55331fa
crazypills <jdyang21@yahoo.com>
2016-04-05 21:59:27 -0700

    Trying out new FastLED library w/ carrion's code

---
+++ Fast_LEDs_combo_w__Accel_code/Fast_LEDs_combo_w__Accel_code.ino
@@ -0,0 +1,173 @@
+#include <Wire.h>
+#include <Accel.h>
+#include <Phase.h>
+#include <FastLED.h>
+
+FASTLED_USING_NAMESPACE
+
+#define ACCEL_INTERVAL_MS 50
+#define PHASE_INTERVAL_MS 50
+Accel accel(ACCEL_INTERVAL_MS);
+Phase phase(PHASE_INTERVAL_MS);
+#if FASTLED_VERSION < 3001000
+#error "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+#define DATA_PIN    6
+//#define CLK_PIN   4
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+#define NUM_LEDS    36
+CRGB leds[NUM_LEDS];
+
+#define BRIGHTNESS          96
+#define FRAMES_PER_SECOND  120
+bool DirectionalThreshold;  //whether the compass threshold can be used
+bool Dance;                 //whether accelerometer is dancing hard enought to be used
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+   accel.begin();
+   pinMode(10, INPUT_PULLUP);
+   pinMode(9, INPUT_PULLUP);
+     // tell FastLED about the LED strip configuration
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  // set master brightness control
+  FastLED.setBrightness(BRIGHTNESS);
+}
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
+
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+  
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  
+   bool didUpdate = accel.Update();
+    phase.update(accel.getLinearAcceleration());
+    FastLED.show();
+    EVERY_N_MILLISECONDS ( 30)
+    {
+      gHue++;
+    }
+    if (accel.isDancing()) {
+//      Single.SetIndex(phase.getPhasePercentage(), phase.getPhaseRatePercentage());
+//      Single.Update();
+//      //Single.ColorSet(Single.Wheel(random(255)));
+//      //Stick.ColorSet(Stick.Color(255, 0, 0));
+//      if (Stick.ActivePattern != FOLLOWER) {
+//        Stick.Follower(Stick.Color(0,255,0),40,2);
+        rainbowWithGlitter();
+     }
+    
+//      Stick.Update();
+     else if (digitalRead(9) == LOW) {
+//      if(Stick.ActivePattern != FOLLOWER)
+//      {
+//        Stick.Follower(Stick.Color(255,0,255),200,5);
+//      }
+//      Stick.Update();
+        sinelon();
+        
+    }
+    // Update the rings.
+    //Single.ActivePattern = RAINBOW_CYCLE;
+    else if (digitalRead(10) == LOW)
+    {
+        confetti();
+    }
+    else
+    { 
+      //Quaternion device = accel.getDeviceOrientation(Quaternion(1, 0, 0));
+      Quaternion device = accel.getAbsoluteOrientation(Quaternion(1, 0, 0));
+      float x = device.b < 0 ? 0 : device.b;
+      float y = device.c < 0 ? 0 : device.c;
+      float z = device.d < 0 ? 0 : device.d;
+      
+//      Single.Color1 = Stick.Color(x*32,y*32, z*32);
+//      Single.Update();
+////      if(Stick.ActivePattern != SCANNER)
+////      {
+////        Stick.Scanner(Stick.Color(0,255,0),70);
+////      }
+//    //Stick.Interval = 20;
+//    Stick.Color1=Stick.Color(x*32,y*32, z*32);
+//      Stick.Update();
+        juggle();
+    }
+}
+
+
+
+
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+void nextPattern()
+{
+  // add one to the current pattern number, and wrap around at the end
+  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+}
+
+void rainbow() 
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+void rainbowWithGlitter() 
+{
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+
+void confetti() 
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = beatsin16(13,0,NUM_LEDS);
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm()
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < NUM_LEDS; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
+
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  byte dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16(i+7,0,NUM_LEDS)] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
+
---
+++ Test_for_Brighter_Lights_w__accel/Test_for_Brighter_Lights_w__accel.ino
@@ -0,0 +1,246 @@
+#include <NeoPatterns.h>
+#include <Wire.h>
+#include <Accel.h>
+
+
+#define ACCEL_INTERVAL_MS 50
+
+// Define some NeoPatterns for the two rings and the stick
+//  as well as some completion routines
+void StickComplete();
+void SingleComplete();
+//void RingComplete();
+
+
+Accel accel(ACCEL_INTERVAL_MS);
+NeoPatterns Stick(24, 6, NEO_GRB + NEO_KHZ800, &StickComplete);
+NeoPatterns Single(1, 8, NEO_GRB + NEO_KHZ800, &SingleComplete);
+//NeoPatterns Ring(24, 12, NEO_GRB + NEO_KHZ800, &RingComplete);
+
+bool DirectionalThreshold;  //whether the compass threshold can be used
+bool Dance;                 //whether accelerometer is dancing hard enought to be used
+bool Lighton;
+unsigned long cycletime = 400;
+unsigned long offtime = 100;
+unsigned long lastCycle;
+
+
+// Initialize everything and prepare to start
+void setup()
+{
+  Serial.begin(115200);
+
+   pinMode(10, INPUT_PULLUP);
+   pinMode(9, INPUT_PULLUP);
+    
+    // Initialize all the pixelStrips
+   Stick.begin();
+   Single.begin();
+   //Ring.begin();
+    //Single.ActivePattern = RAINBOW_CYCLE;
+    Single.RainbowCycle(240, FORWARD);
+    Single.setBrightness(50);
+	  accel.begin();
+    //Ring.setBrightness(127);
+     Stick.setBrightness(255);
+
+
+    lastCycle = millis();
+    //setup the stick with red
+    //Stick.Scanner(Stick.Color(255,0,0), 200);
+    Stick.ColorSet(Stick.Color(255,0,0));
+    //Ring.ColorSet(Ring.Color(0,255,0));
+}
+
+// Main loop
+void loop()
+{
+    // Read the sensors
+
+    // Update the rings.
+    //Single.Update();    
+	  accel.Update();
+    //Stick.SetIndex((accel.getPhase()+PI)/(2*PI));
+    //Stick.Interval=((int)((accel.getPhaseRate()+PI)/(2*PI) * 150));
+
+//    if ( millis() - lastCycle > cycletime)
+//    {
+//      lastCycle = millis();
+//      //Ring.SetBlank(true);
+//      //Stick.SetBlank(true);
+//      Stick.BrightLights();
+//    }
+//    if (millis() - lastCycle > offtime)
+//    {
+//      //Ring.SetBlank(false);
+//      //Stick.SetBlank(false);
+//      Stick.NormalLights();
+//    }
+    /*
+    if (accel.getPhase() > 0)
+    {
+      Stick.ColorSet(Stick.Color(255,0,0));
+    }
+    else
+    {
+      Stick.ColorSet(Stick.Color(0,255,255));
+    }
+    */
+    Stick.floatIndexRate = 1.0 / Stick.TotalSteps;
+    //Ring.floatIndexRate = 1.0/ Ring.TotalSteps;
+    if (accel.isDancing())
+    {
+      Single.ColorSet(Single.Wheel(random(255)));
+      //Stick.ColorSet(Stick.Wheel(accel.GetCompassReading()));
+      Stick.ColorSet(Stick.Color(255, 0, 0));
+      if(Stick.ActivePattern != HALFUPDOWN)
+      {
+        Stick.HalfUpDown(Stick.Color(0,255,0),10);
+        Stick.SetIndex((accel.getPhase()+PI)/(2*PI));
+      }
+      Stick.floatIndexRate = accel.getPhaseRate()/(2*PI);
+      Stick.Update();
+    }
+    else if (digitalRead(9) == LOW)
+    {
+      //Stick.SetBlank(true);
+//      if(Ring.ActivePattern != THEATER_CHASE)
+//      {
+//        Ring.TheaterChase(Ring.Color(255,0,255),Ring.Color(0,255,255),80,FORWARD);
+//      }
+      
+      if(Stick.ActivePattern != FOLLOWER)
+      {
+        Stick.Follower(Stick.Color(255,0,255),300,5);
+      }
+      //Ring.Update();
+      Stick.Update();
+    }
+    // Update the rings.
+    //Single.ActivePattern = RAINBOW_CYCLE;
+    else if (digitalRead(10) == LOW)
+    {
+      //Stick.SetBlank(false);
+//      if(Ring.ActivePattern != HALFUPDOWN)
+//      {
+//        Ring.HalfUpDown(random(255),250);
+//      }
+      if(Stick.ActivePattern != DOUBLESCANNER)
+      {
+        Stick.DoubleScanner(random(255),40);
+      }
+//      Ring.Update();
+      Stick.Update();
+    }
+    else
+    { 
+//      Stick.SetBlank(false);
+      Single.Update();
+      if(Stick.ActivePattern != RAINBOW_CYCLE)
+      {
+        //Stick.Fade(Stick.Color(255,0,0),Stick.Color(0,255,255),120,50,FORWARD);
+        //Stick.RunningRainbow(Stick.Wheel(random(255)),120);
+        //Stick.TheaterChase(Stick.Color(0,255,255),Stick.Color(255,0,255), 250 , FORWARD);
+        //Stick.HalfUpDown(Stick.Color(0,255,255),40);
+        //Stick.ColorWipe(Stick.Color(0,255,255),20,FORWARD);
+        //Stick.Scanner(Stick.Color(0,255,255),40);
+        //Stick.Twinkle(45,200);
+        Stick.RainbowCycle(20, FORWARD);
+      }
+//      if(Ring.ActivePattern != RAINBOW_CYCLE)
+//      {
+//        Ring.RainbowCycle(20, FORWARD);
+//      }
+    //Stick.Interval = 20;
+      Stick.Update();
+//      Ring.Update();
+    }
+    /*
+    // Switch patterns on a button press:
+    if (digitalRead(8) == LOW) // Button #1 pressed
+    {
+        // Switch Ring1 to FASE pattern
+        //Ring1.ActivePattern = FADE;
+        //Ring1.Interval = 20;
+        // Speed up the rainbow on Ring2
+        //Ring2.Interval = 0;
+        // Set stick to all red
+        Stick.ColorSet(Stick.Color(255, 0, 0));
+    }
+    else if (digitalRead(9) == LOW) // Button #2 pressed
+    {
+        // Switch to alternating color wipes on Rings1 and 2
+        //Ring1.ActivePattern = COLOR_WIPE;
+        //Ring2.ActivePattern = COLOR_WIPE;
+        //Ring2.TotalSteps = Ring2.numPixels();
+        // And update tbe stick
+        switch(Stick.ActivePattern)
+            {
+                case RAINBOW_CYCLE:
+                    Stick.ActivePattern = THEATER_CHASE;
+                    break;
+                case THEATER_CHASE:
+                    Stick.ActivePattern = COLOR_WIPE;
+                    break;
+                case COLOR_WIPE:
+                    Stick.ActivePattern = SCANNER;
+                    break;
+                case SCANNER:
+                    Stick.ActivePattern = FADE;
+                    break;
+                case FADE:
+                    Stick.ActivePattern = COMPASS;
+                    break;
+                case COMPASS:
+                    Stick.ActivePattern = RAINBOW_CYCLE;
+                    break;
+                default:
+                    break;
+            }
+            //Stick.ActivePattern = Stick.ActivePattern + 1;
+        Stick.Update();
+    }
+    else // Back to normal operation
+    {
+        Stick.Update();
+    }    
+    */
+}
+
+//------------------------------------------------------------
+//Completion Routines - get called on completion of a pattern
+//------------------------------------------------------------
+
+// Stick Completion Callback
+void StickComplete()
+{
+    // Random color change for next scan
+    if (Stick.ActivePattern == HALFUPDOWN)
+    {
+      Stick.Color1 = Stick.Wheel(random(255));
+      Stick.clear();
+      Stick.show();
+    }
+    else
+    {
+      Stick.Color1 = Stick.Wheel(random(255));
+    }
+}
+
+void SingleComplete()
+{
+    // Random color change for next scan
+    //Stick.Color1 = Stick.Wheel(random(255));
+}
+//void RingComplete()
+//{
+//  if (Stick.ActivePattern == HALFUPDOWN)
+//    {
+//      Stick.Color1 = Stick.Wheel(random(255));
+//      Stick.clear();
+//      Stick.show();
+//    }
+//    Ring.Color1 = Stick.Wheel(random(255));
+//}
+//
+
